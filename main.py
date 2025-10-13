6cd85aca85f51bf88d4cb47abc616479ad529951
StackOverflowed512 <omkarmutyalwar5@gmail.com>
2025-10-13 18:28:59 +0530

    change

--- main.py
+++ main.py
@@ -150,3 +150,8 @@ def extract_customer_data(history):
             break
     
     return data
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
